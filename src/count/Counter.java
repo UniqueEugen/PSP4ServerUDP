b1c87c53d77fcf47209d43ad6ee9fa67395ec9b8
UniqueEugen <112904113+UniqueEugen@users.noreply.github.com>
2023-10-27 23:07:52 +0300

    Git push

--- src/count/Counter.java
+++ src/count/Counter.java
@@ -1,2 +1,36 @@
-package count;public class Counter {
+package count;
+
+import myLibrary.console.Console;
+
+import java.io.IOException;
+
+public class Counter {
+    private double x;
+    private double y;
+    private double z;
+    private Double answer;
+    public Counter(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public Counter(String xyz){
+        String XYZ[] = xyz.split(" ");
+        try {
+            this.x = Double.parseDouble(XYZ[0]);
+            this.y = Double.parseDouble(XYZ[1]);
+            this.z = Double.parseDouble(XYZ[2]);
+        }catch (Exception ex){
+            Console.log("Exeption! "+ ex);
+        }
+    }
+    public String  count(){
+        Console.log("X " + x + " Y " + y + " Z "+ z);
+        answer = 6+((Math.pow(Math.E,(x-Math.sin(y))))/
+                (y+((Math.tan(Math.pow(x,2)))/
+                        (y+(Math.pow(x,7)/
+                                z))))*
+                (Math.pow((1+(Math.pow((1.0/Math.tan(z/100)),7))),Math.sqrt(Math.abs(y)+3))));
+        return answer.toString();
+    }
 }
--- src/server/UDPServer.java
+++ src/server/UDPServer.java
@@ -1,2 +1,61 @@
-package server;public class UDPServer {
+package server;
+
+import count.Counter;
+import myLibrary.console.Console;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDPServer {
+    public final static int DEFAULT_PORT = 8001;
+    public final String QUIT_CMD = "QUIT";
+    public final String COUNT_CMD = "COUNT";
+    public final String UNKNOWN_CMD = "Unknown command";
+
+    public void runServer() throws IOException {
+        DatagramSocket socket = null;
+        try {
+            boolean stopFlag = false;
+            byte[] buf;
+            socket = new DatagramSocket(DEFAULT_PORT);
+            Console.log("UDPServer: Started on " + socket.getLocalAddress() + ":"
+                    + socket.getLocalPort());
+            while (!stopFlag) {
+                buf = new byte[194];
+                DatagramPacket recvPacket = new DatagramPacket(buf, buf.length);//создание объекта дейтаграммы для получения данных
+                socket.receive(recvPacket);
+                String cmd = new String(recvPacket.getData()).trim();
+                Console.log("UDPServer: Command: " + cmd);
+                DatagramPacket sendPacket = new DatagramPacket(
+                        buf, 0, recvPacket.getAddress(), recvPacket.getPort()
+                );
+                int n = 0;//количество байт в ответе
+                if (cmd.equals(COUNT_CMD)) {//проверка версии команды
+                    socket.receive(recvPacket);
+                    String DATA = new String(recvPacket.getData()).trim();
+                    Counter counter = new Counter(DATA);
+                    String answer = counter.count();
+                    n = answer.getBytes().length;
+                    System.arraycopy(answer.getBytes(), 0, buf, 0, n);
+                } else if (cmd.equals(QUIT_CMD)) {
+                    stopFlag = true;//остановка сервера
+                    continue;
+                } else {
+                    n = UNKNOWN_CMD.getBytes().length;
+                    System.arraycopy(UNKNOWN_CMD.getBytes(), 0, buf, 0, n);
+                }
+                sendPacket.setData(buf);//установить массив посылаемых данных
+                sendPacket.setLength(n);//установить длину посылаемых данных
+                socket.send(sendPacket);//послать сами данные
+            } // while(server is not stopped)
+            Console.log("UDPServer: Stopped");
+        }
+        finally {
+            if (socket != null) {
+                socket.close();//закрытие сокета сервера
+            }
+        }
+    }
+
 }
